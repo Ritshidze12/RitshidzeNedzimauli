e3d5aee55b28e3edb891b6d475d942c8bbf1dbe9
Ritshidze Nedzimauli <u24717739@up.ac.za>
2025-02-28 12:44:50 +0200

    INF272

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using RitshidzeNedzimauliStudyGroup.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RitshidzeNedzimauliStudyGroup.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult ListPeople()
+        {
+            List<Models.MyStudents> students = new List<Models.MyStudents>();
+
+            students.Add(new Models.MyStudents { StudentNumber = "u24717739", FirstName = "Ritshidze", surname = "Nedzimauli", email = "u24717739@tuks.co.za" });
+            students.Add(new Models.MyStudents { StudentNumber = "u24784606", FirstName = "Msizi", surname = "Gumede", email = "u24784606@tuks.co.za" });
+            students.Add(new Models.MyStudents { StudentNumber = "u24634400", FirstName = "Boitshoko", surname = "Mphahlele", email = "u24634400@tuks.co.za" });
+            students.Add(new Models.MyStudents { StudentNumber = "u24", FirstName = "Gundo", surname = "Ndou", email = "u24@tuks.co.za" });
+            students.Add(new Models.MyStudents { StudentNumber = "u24778899", FirstName = "Bono", surname = "Mudau", email = "u24778899@tuks.co.za" });
+
+            return View(students);
+        }
+    }
+}
---
+++ Models/MyStudents.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RitshidzeNedzimauliStudyGroup.Models
+{
+	public class MyStudents
+	{
+		[Display(Name = "Sudent Number")]
+		public string StudentNumber { get; set; }
+
+		[Display(Name = "First Name")]
+		public string FirstName { get; set; }
+
+		[Display(Name= "Surname")]
+		public string surname { get; set; }
+
+		[Display(Name = "Email")]
+		public string email { get; set; }
+	}
+}
