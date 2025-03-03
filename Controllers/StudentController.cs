b66da20918114ef4b7a1f46490376dfac093eaf1
Ritshidze Nedzimauli <u24717739@up.ac.za>
2025-03-03 10:11:06 +0200

    Ritshidze

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -10,15 +10,15 @@ namespace RitshidzeNedzimauliStudyGroup.Controllers
     public class StudentController : Controller
     {
         // GET: Student
-        public ActionResult ListPeople()
+        public ActionResult StudentsView()
         {
             List<Models.MyStudents> students = new List<Models.MyStudents>();
 
             students.Add(new Models.MyStudents { StudentNumber = "u24717739", FirstName = "Ritshidze", surname = "Nedzimauli", email = "u24717739@tuks.co.za" });
             students.Add(new Models.MyStudents { StudentNumber = "u24784606", FirstName = "Msizi", surname = "Gumede", email = "u24784606@tuks.co.za" });
             students.Add(new Models.MyStudents { StudentNumber = "u24634400", FirstName = "Boitshoko", surname = "Mphahlele", email = "u24634400@tuks.co.za" });
-            students.Add(new Models.MyStudents { StudentNumber = "u24", FirstName = "Gundo", surname = "Ndou", email = "u24@tuks.co.za" });
-            students.Add(new Models.MyStudents { StudentNumber = "u24778899", FirstName = "Bono", surname = "Mudau", email = "u24778899@tuks.co.za" });
+            students.Add(new Models.MyStudents { StudentNumber = "u24724918", FirstName = "Gundo", surname = "Ndou", email = "u24724918@tuks.co.za" });
+            students.Add(new Models.MyStudents { StudentNumber = "u24924142", FirstName = "Walter", surname = "Marimi", email = "u24924142@tuks.co.za" });
 
             return View(students);
         }
